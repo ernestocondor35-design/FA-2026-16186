6388024b7fd93963bdee7af7387475b64020be9d
Leandro <ernestocondor.35@gmail.com>
2026-04-10 10:22:02 -0500

    subiendo Ejercicio1 en c#

---
+++ SE3-C/Ejercicio1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3_C
+{
+    internal class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            string nombre, carrera;
+
+            Console.Write("Ingrese nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("\nombreIngrese carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine("\n-------- IMPRESIÓN CON + --------");
+            Console.WriteLine(nombre + ", Bienvenido al curso FA de la carrera "+carrera);
+
+            Console.WriteLine("\n-------- IMPRESIÓN CON $ --------");
+            Console.WriteLine($"{nombre}, Bienvenido al curso FA de la carrera {carrera}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
