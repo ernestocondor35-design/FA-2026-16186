2ab91fccb0a27ced9a269876ef249e1836754197
Leandro <ernestocondor.35@gmail.com>
2026-04-10 10:38:34 -0500

    subiendo Ejercicio1 en py

---
+++ SE3-P/Ejercicio1.py
@@ -0,0 +1,8 @@
+nombre =input("Ingrese tu nombre: ")
+carrera =input("Ingrese tu crrera: ")
+
+print("\n-------- IMPRESIÓN CON + --------");
+print(nombre + ", Bienvenido al curso FA de la carrera "+carrera);
+
+print("\n-------- IMPRESIÓN CON $ --------");
+print(f"{nombre}, Bienvenido al curso FA de la carrera {carrera}");
