60a9c84f8a84d676ee2c4a611553ae2490964331
Leandro <ernestocondor.35@gmail.com>
2026-04-17 10:35:58 -0500

    avance de ejercicio en py hasta el3

---
+++ SE3-P/Ejercicio3.py
@@ -0,0 +1,7 @@
+num1 = int(input("ingrese numero 1: "));
+num2 = int(input("ingrese numero 2: "));
+
+print(f"\nSuma: {num1 + num2}");
+print(f"Resta: {num1 - num2}");
+print(f"Multiplicación: {num1 * num2}");
+print(f"División: {num1 / num2}");
